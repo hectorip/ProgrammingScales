6a4baa101cf487a7eb068a39992f4856ce6533a6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-22 23:56:11 -0600

    mode

--- elixir/ten_days_of_statistics/one.ex
+++ elixir/ten_days_of_statistics/one.ex
@@ -6,12 +6,24 @@ defmodule Staitstics do
   def mean(n, values) do
     values
       |> Enum.sum
-      |> &(&1/n)
+      |> Kernel./(n)
+      |> Float.round(1)
   end
 
-  def mode do
-    :ok
+  def mode(n, values) do
+    initial = %{}
+    Enum.reduce(initial, fn val, acc ->
+      case Map.get(acc, val) do
+        nil ->
+          Map.put(acc, val, 1)
+        total ->
+          Map.put(acc, val, total + 1)
+      end
+    end)
+    |> Enum.sort(fn {k1, v1}, {k2, v2} -> v1 >= v2 end)
+    |> Enum.at(0)
   end
 end
 
-Staitstics(5, [1,2,3,4,5])
+Staitstics.mean(5, [1,2,3,4,5])
+Staitstics.mode(5, [1, 0, 0, 2, 3])
